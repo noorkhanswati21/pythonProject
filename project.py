553e374074a35c20c2373f000dcbdd5ae53e75ae
noorkhanswati21 <aliyanyamin42@gmail.com>
2024-08-11 06:42:54 -0700

    program done

--- project.py
+++ project.py
@@ -9,9 +9,26 @@ def disply_stock():
       print(f"{item}:{quantity}")
 disply_stock()
 def add_item(item,quantity):
-   if item in classroom_stocks:
+   if item in classroom_stock:
       classroom_stock[item]+=quantity
    else:
       classroom_stock[item]=quantity
-add_item(item,quantity)
+add_item
+def remove_item(item,quantity):
+   if item in classroom_stock:
+      if classroom_stock[item]>=quantity:
+         classroom_stock[item]-=quantity
+      else:
+         print("Not enough stock to remove.")
+   else:
+      print("item not found in stock.")
+remove_item
+#Main program
+disply_stock()
+add_item("markers",10)
+remove_item("pencil",5)
+disply_stock()
 
+
+
+     
